b00febeca4d317c452e923e7601964c7784a5bfe
omar-alsawaf <99086491+omar-alsawaf@users.noreply.github.com>
2022-10-10 18:52:35 +0200

    updated way

--- send messages to mobile no read from an excel sheet/whatsapp.py
+++
@@ -1,9 +0,0 @@
-import pywhatkit
-from openpyxl import load_workbook
-wb = load_workbook('numbers.xlsx')
-sh1 = wb['Sheet1']
-k = 0
-for i in range(1,4):
-    Numbr = '+20'+str(sh1.cell(i,1).value)
-    pywhatkit.sendwhatmsg(Numbr,"PLEASE IGNORE",22,46+k,10,True,10)
-    k+= 3
